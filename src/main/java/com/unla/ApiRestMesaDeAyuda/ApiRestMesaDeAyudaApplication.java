d08e32a901c6cb892209c450454d26ab34d758e5
facundo cusato <facundocusato12@gmail.com>
2021-11-13 11:43:04 -0300

    Adding files

---
+++ src/main/java/com/unla/ApiRestMesaDeAyuda/ApiRestMesaDeAyudaApplication.java
@@ -0,0 +1,13 @@
+package com.unla.ApiRestMesaDeAyuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMesaDeAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMesaDeAyudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/ApiRestMesaDeAyuda/ApiRestMesaDeAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.ApiRestMesaDeAyuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMesaDeAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
